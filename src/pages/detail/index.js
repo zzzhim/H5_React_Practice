a76bc95a57f37bce513c0469a0b810605a6a1748
zzzhim <3218566389@qq.com>
2019-05-04 21:36:35 +0800

    创建详情页面 && 使用redux管理详情页面数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -3,17 +3,32 @@
  * @Author: your name
  * @LastEditors: Please set LastEditors
  * @Date: 2019-05-02 20:48:04
- * @LastEditTime: 2019-05-03 02:08:37
+ * @LastEditTime: 2019-05-04 21:36:03
  */
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+    DetailWrapper,
+    Header,
+    Content
+} from './style';
 
 class Detail extends Component {
     render() {
+        const { title, content } = this.props;
         return (
-            <div>detail</div>
+            <DetailWrapper>
+                <Header>{ title }</Header>
+                <Content dangerouslySetInnerHTML={{ __html: content }} />
+            </DetailWrapper>
         );
     }
 };
 
-export default Detail;
+const mapState = (state) => ({
+    title: state.getIn([ 'detail', 'title' ]),
+    content: state.getIn([ 'detail', 'content' ]),
+});
+
+export default connect(mapState, null)(Detail);
